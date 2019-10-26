bb588c9ad4e1c09b9d66a866d68ffaee089db248
Oral Shane Curtis (Valhalla) <andifsar@hotmail.com>
2019-10-26 10:26:39 -0400

    2019-10-26:1025 - [moved some module funcitons to a creator.ts file, added imports (auto)]

---
+++ src/creator.ts
@@ -0,0 +1,37 @@
+import { ConfigurationTarget, workspace, window } from 'vscode';
+export async function getQtCreatorPath() {
+	let pathUri = await window.showOpenDialog({
+		canSelectFolders: false,
+		canSelectFiles: true,
+		canSelectMany: false, openLabel: 'Select the QtCreator executable'
+	});
+	if (!pathUri) {
+		return null;
+	}
+	let creatorPath = pathUri[0].fsPath;
+	const settings = await workspace.getConfiguration('launchqtcreator');
+	settings.update('qtCreatorPath', creatorPath, ConfigurationTarget.Global).then(() => {
+		console.log('path from OpenDialog ' + creatorPath);
+		if (doLaunchQtCreator(creatorPath)) {
+			window.showInformationMessage("Launching QtCreator from [" + creatorPath + "]");
+		}
+		else {
+			window.showErrorMessage("Error launching QtCreator from [" + creatorPath + "]");
+		}
+	}).then(undefined, err => {
+		window.showErrorMessage('unable to set \"launchqtcreator.qtCreatorPath\"');
+	});
+	return creatorPath;
+}
+
+export async function doLaunchQtCreator(qtcreator: any) {
+	const cp = require('child_process');
+	await cp.exec(qtcreator, (err: string, stdout: string) => {
+		if (err) {
+			console.log('error: ' + err);
+		}
+		if (stdout) {
+			console.log('stdout: ' + stdout);
+		}
+	});
+}
--- src/extension.ts
+++ src/extension.ts
@@ -2,10 +2,10 @@
 // Import the module and reference it with the alias vscode in your code below
 import { StatusBarAlignment,
 		 ExtensionContext, 
-		 ConfigurationTarget, 
 		 workspace, 
 		 window, 
 		 commands } from 'vscode';
+import { getQtCreatorPath, doLaunchQtCreator } from './creator';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -33,7 +33,7 @@ export function activate(context: ExtensionContext) {
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() { }
+export function deactivate() {}
 
 export async function LaunchQtCreator() : Promise<boolean> {
 	let config = workspace.getConfiguration('launchqtcreator');
@@ -43,7 +43,7 @@ export async function LaunchQtCreator() : Promise<boolean> {
 	{		
 		qtcreator = getQtCreatorPath().then(()=>{ 			
 			return_value = true;
-		}).then(undefined, err =>{
+		}).then(undefined, () =>{
 			return_value = false;
 		});			
 	}
@@ -52,48 +52,9 @@ export async function LaunchQtCreator() : Promise<boolean> {
 		await doLaunchQtCreator(qtcreator).then(()=>{
 			window.showInformationMessage("Launching QtCreator from [" + qtcreator + "]");
 			return_value = true;
-		}).then(undefined, err=>{
+		}).then(undefined, ()=>{
 			window.showErrorMessage("Error launching QtCreator from [" + qtcreator + "]");
 		});
 	}	
 	return return_value;
-}
-
-async function doLaunchQtCreator(qtcreator: any) {
-	const cp = require('child_process');
-	await cp.exec(qtcreator, (err: string, stdout: string, stderr: string) => {
-		if (err) { console.log('error: ' + err); }
-		if (stdout) {console.log('stdout: ' + stdout);}	
-	});
-}
-
- async function getQtCreatorPath() 
- {
-	let pathUri = await window.showOpenDialog({
-	canSelectFolders: false,
-		canSelectFiles: true,
-		canSelectMany: false, openLabel: 'Select the QtCreator executable'
-	});
-	if(!pathUri)
-	{
-		return null;
-	}
-
-	let creatorPath = pathUri[0].fsPath;
-
-	const settings = await workspace.getConfiguration('launchqtcreator');		
-	settings.update('qtCreatorPath',creatorPath,ConfigurationTarget.Global).then(()=>{
-		console.log('path from OpenDialog ' + creatorPath);
-		if(doLaunchQtCreator(creatorPath))
-		{
-			window.showInformationMessage("Launching QtCreator from [" + creatorPath + "]");
-		}
-		else
-		{
-			window.showErrorMessage("Error launching QtCreator from [" + creatorPath + "]");
-		}
-		}).then(undefined, err=>{
-			window.showErrorMessage('unable to set \"launchqtcreator.qtCreatorPath\"');
-		});	
-	return creatorPath;
 }
