bad86334a8775b98ccb04d62915deaa60900c609
Oral Shane Curtis (Valhalla) <andifsar@hotmail.com>
2019-10-27 18:26:15 -0400

    recovered from the git-bash delete non existent folder that turned out to try and delete any slightly matching pattern blunder

--- update_vsix.sh
+++ update_vsix.sh
@@ -4,12 +4,14 @@ script=`basename $0`
 new_name=$1
 old_names=$(ls ./latest/)
 
+test -d ./Releases || mkdir ./Releases
+
 for name in ${old_names}; do
     mv ./latest/${name} ./Releases
 done
 
-[ "${new_name}xx" == "xx" ] && { 
-    echo "specify new name"; 
+[ "${new_name}xx" == "xx" ] && {
+    echo "specify new name";
     exit -1
 }|| {
     vsce package -o ${new_name}
