ca265f1f754abac4d4f01f966a8621fc938d0a71
Oral Shane Curtis (Valhalla) <andifsar@hotmail.com>
2019-10-27 10:35:36 -0400

    2019-10-27:1034 - [moved the 'var path' import to top of file to be globally available to all methods]

--- src/extension.ts
+++ src/extension.ts
@@ -7,6 +7,7 @@ import { getQtCreatorPath,
 		 getQtDesignerPath,
 		 doLaunchInQtDesigner} from './creator';
 
+var path = require("path");				 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
