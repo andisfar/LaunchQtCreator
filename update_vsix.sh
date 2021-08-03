a1eeb0b681584d22b1f151b7002561340b92818e
Oral Shane Curtis (AngryDane) <andisfar@hotmail.com>
2021-08-03 06:44:27 -0400

    fixed some errors and modified behavior

--- update_vsix.sh
+++ update_vsix.sh
@@ -5,20 +5,26 @@ old_names=$(ls ./latest/)
 
 test -d ./Releases || mkdir ./Releases
 
-for name in ${old_names}; do
+for name in ${old_names[@]}; do
     test "./latest/${name}" == "./latest/${new_name}" && {
         echo "remove old file with same name..."
-        rm -f ${nme}
+        rm -f ${name}
      } ||{
         echo "move previous file to ./Releases"
         mv -f ./latest/${name} ./Releases
      }
 done
 
-echo "generating new package..."
-vsce package
-# echo "publishing..."
-cat ../.extensions | vsce publish
-echo "move new package into latest..."
+echo -n "generating new package..."
+vsce package &> /dev/null
+echo "done"
+
+echo -n "publishing..."
+cat ../.extensions | vsce publish &> /dev/null
+echo "done"
+
+echo -n "move new package into latest..."
 mv *.vsix ./latest/
+echo "done"
+
 exit 0
